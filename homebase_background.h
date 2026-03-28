2bb674340d85040ac547eda5a5a55f4abef8c119
kaylee162 <kayleehenry162@gmail.com>
2026-03-28 17:29:03 -0400

    added tilemap .c and .h file info

---
+++ homebase_background.h
@@ -0,0 +1,10 @@
+#ifndef HOMEBASE_BACKGROUND_H
+#define HOMEBASE_BACKGROUND_H
+
+#define HOMEBASE_BACKGROUND_WIDTH  (32)
+#define HOMEBASE_BACKGROUND_HEIGHT (64)
+#define homebase_backgroundLen (4096)
+
+extern const unsigned short homebase_backgroundMap[2048];
+
+#endif
